71996c671fc465a2c5b51768c450953da37ce2e2
Kimakim322 <kimakim94@mail.ru>
2017-11-04 01:44:44 +0400

    first proj

---
+++ 123.cpp
@@ -0,0 +1,8 @@
+http://rm.therdteam.com/issues/48
+http://www.jetsonhacks.com/2016/11/20/single-chip-lidar-vl53l0x-nvidia-jetson-dev-kits/
+https://github.com/Kimakim322/jetson
+http://www.netsarang.com/download/down_xsh5.html?token=NUlIa0RRTk81Z21SRkJxQzJiaWt2QUBkVEtSNWl0YlVYb3A0a3VXY3BLT2JR
+
+Инструкция:
+http://dev-blogs.com/git-hub-how-to-upload/
+https://kuroikaze85.wordpress.com/2009/12/16/git-on-windows/
